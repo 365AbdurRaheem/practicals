83229b07f5c6d0a99db0f45c69809738663878c1
Raja Abdur Raheem <130824814+365AbdurRaheem@users.noreply.github.com>
2024-03-22 22:01:48 +0500

    added

---
+++ index.js
@@ -0,0 +1,35 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const fs=require("fs");
+
+const PORT = process.env.PORT || 3000;
+
+const app=express();
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.post("/fb", (req, res) => {
+    let feedback="| "+req.body.feedback+"\n";
+    fs.appendFile("feedback.txt", feedback, (err) => {
+        if (err)
+          console.log(err);
+        else
+          res.redirect("/");   
+      });
+});
+
+app.get("/", (req, res) => {
+    res.set({
+        "Allow-access-Allow-Origin":"*"
+    })
+    return res.redirect("index.htm");
+
+}).listen(PORT);
+console.log("Listening on port "+PORT);
+
+
+
