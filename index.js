d2d612793407506e7e025ff5383ca0dcff90e7d4
Raja Abdur Raheem <130824814+365AbdurRaheem@users.noreply.github.com>
2024-03-24 20:00:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-
+const cors=require("cors");
 const PORT = process.env.PORT || 3000;
 
 const app=express();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
